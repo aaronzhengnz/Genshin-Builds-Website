be87e87861fa60445c39eb17776b0bb5d10fc6a0
Aaron Zheng <21211@burnside.school.nz>
2024-05-15 14:03:26 +1200

    Added teams route

--- routes.py
+++ routes.py
@@ -11,3 +11,21 @@ def Menu():
     cur.execute('SELECT * FROM users')
     menu = cur.fetchall()
     return render_template('menu.html', menu=menu)
+
+
+@app.route('/teams/')
+def Teams():
+    conn = sqlite3.connect('database.db')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM teams')
+    teams = cur.fetchall()
+    return render_template('teams.html', teams=teams)
+
+
+@app.route('/teams/<int:id>')
+def Team(id):
+    conn = sqlite3.connect('database.db')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM teams WHERE id = ?', (id,))
+    team = cur.fetchone()
+    return render_template('team.html', team=team)

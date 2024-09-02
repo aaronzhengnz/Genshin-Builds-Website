d22a9bb372be34e19e6b375910f256a055572683
Aaron Zheng <21211@burnside.school.nz>
2024-09-03 11:42:06 +1200

    Fixed positioning of hamburger menu button

--- static/nav.js
+++ static/nav.js
@@ -1,9 +1,6 @@
 const stackMenu = document.querySelector('.stack-menu');
 const hiddenMenu = document.querySelector('.hidden-menu');
 
-console.log(stackMenu); // Should log the stackMenu element
-console.log(hiddenMenu); // Should log the hiddenMenu element
-
 stackMenu.addEventListener('click', () => {
   stackMenu.classList.toggle('active');
   hiddenMenu.classList.toggle('active');
